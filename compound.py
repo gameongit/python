fb68304818f5b504b638f1fd97f19aabe406fa6b
gameongit <lalitsharma12345@gmail.com>
2018-10-30 16:37:49 +0100

    adding more details

--- compound.py
+++ compound.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
+from __future__ import division
 
-princ = input()
+print ("Enter the amount:")
+Princ = int(input())
+print("Enter the rate of interest:")
+RateOfInterest = int(input())
+print("Enter the number of years:")
+Years = int(input())
+amount = int(Princ * pow(((100 + RateOfInterest) / 100), Years))
+print('The CI for this amount is :', amount)
+#print("The CI for the principle ", Princ, " for ", Years, "on ", RateOfInterest, "rate of interes is: ", amount)
+
