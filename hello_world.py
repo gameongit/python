b40231793a11fe2c319b94d4547923713ee3f694
gameongit <lalitsharma12345@gmail.com>
2019-01-06 19:55:37 +0000

    Hello-change

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+
+def hello_world():
+   n = 0
+   while True:
+      print ("This is a test check " + str(n))
+      n += 1
+      if n > 9:
+         break
+
+
+#def hello_world1():
+#    while True():
+#        print ("Hello World1 !!!")
+#        break
+
+hello_world()
