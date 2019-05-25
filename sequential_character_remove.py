e213e451fe70c2a59fdd3eb067b4bf8777a8d0ac
gameongit <lalitsharma12345@gmail.com>
2019-05-25 17:20:20 +0000

    sequential character remove

--- sequential_character_remove.py
+++ sequential_character_remove.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python
-
+import sys
+import random, string
 DOCSTRING=''' To remove the sequential duplicate characters in the lis.
 original = [a, x, b, b, x, d]
 output = [a, d]
 '''
-LIST = ["a", "x", "b", "b", "x", "d"]
+LIST = []
+for i in range(20):
+    LIST.append(random.choice(string.ascii_lowercase))
+
+LIST = ['y', 'w', 'w', 'u', 'j', 'p', 'm', 'f', 'm', 'a', 'd', 'b', 'k', 'p', 'a', 'u', 't', 'y', 'n', 'j']
 print LIST
 
 def inner_repeat_loop():
-    for i in range(len(LIST)):
-        if i < (len(LIST)-1) and LIST[i] == LIST[i+1]:
-             LIST.remove(LIST[i+1])
-             LIST.remove(LIST[i])
-        elif i == (len(LIST)-1) and LIST[i-1] == LIST[i]:
-             LIST.remove(LIST[i-1])
-             LIST.remove(LIST[i])
+   i = 0
+   while i < len(LIST)-1:
+      if LIST[i] == LIST[i+1]:
+          LIST.remove(LIST[i+1])
+          LIST.remove(LIST[i])
+          i -= 1
+      i += 1
 
 def sequential_char_remove():
+    if len(LIST) == 0 or len(LIST) == 1:
+        print("The list is not complete to sort")
+        exit()
+    RANLIST = list(LIST)
     inner_repeat_loop()
-    inner_repeat_loop()
+    while RANLIST != LIST:
+        RANLIST = list(LIST)
+        inner_repeat_loop()
     return LIST
 
 print(sequential_char_remove())
